953d917a64314690b918ddc2ea092afc1b463e10
NTh-Hai <thehai318@gmail.com>
2023-03-18 03:20:24 +0700

    fix: making firebase api public

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-mimic-bfdca.firebaseapp.com",
   projectId: "twitter-mimic-bfdca",
   storageBucket: "twitter-mimic-bfdca.appspot.com",
